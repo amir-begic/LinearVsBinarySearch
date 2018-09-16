869ffb5d3499ec370461121cb7dce82b2859612c
Amir Begic <amir.begic@namics.com>
2018-09-16 15:39:24 +0200

    Initial commit with array creation and shuffling

---
+++ LinearVsBinarySearch/ArrayHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearVsBinarySearch
+{
+    class ArrayHandler
+    {
+        private int[] shuffledArray;
+        public int[] GenerateSortedArray()
+        {
+            int[] sortedArray = new int[50];
+
+            for (int i = 0; i < 50; i++)
+            {
+                sortedArray[i] = i;
+            }
+
+            return sortedArray;
+        }
+
+        public int[] ShuffleArray(int[] sortedArray)
+        {
+            shuffledArray = sortedArray;
+            Random r = new Random();
+            Shuffle(shuffledArray, shuffledArray.Length, r);
+            
+            return shuffledArray;
+        }
+
+        private void Shuffle(int[] sortedArray, int length, Random r)
+        {
+            if (length == 0)
+            {
+                return;
+            }
+            
+            int index = r.Next(0, length);
+            
+            int temp = sortedArray[length - 1];
+            sortedArray[length - 1] = sortedArray[index];
+            sortedArray[index] = temp;
+            
+            Shuffle(sortedArray, length - 1, r);
+        }
+
+
+
+    }
+}
---
+++ LinearVsBinarySearch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearVsBinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ah = new ArrayHandler();
+            var sortedArray = ah.GenerateSortedArray();
+            var unsortedArray = ah.ShuffleArray(ah.GenerateSortedArray());
+        }
+    }
+}
